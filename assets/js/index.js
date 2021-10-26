d3125efd0453512003d489957b51611de492d0bd
morghull <morghull@gmail.com>
2021-10-26 11:07:46 +0300

    add slider structure

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,14 @@
 'use strict';
 
-const img1 = document.querySelector(
-  'div.wrap>section>article+article>img'
+const imagesDB = [
+  'https://karma-kollection.com/wp-content/uploads/2018/05/magical-banner.jpg',
+  'https://fanaticcook.files.wordpress.com/2021/04/animals.jpg',
+  'https://i.pinimg.com/originals/7e/1c/0b/7e1c0b3223789770299bc3b66b2fc2a0.jpg',
+  'https://thypix.com/wp-content/uploads/2018/05/Sommerlandschaft-Bilder-1.jpg',
+  'https://i.pinimg.com/originals/2c/84/93/2c8493d6251661e82b62fc6ab518e7e3.jpg'
+];
+
+const img = document.querySelector('.slide>img');
+const [buttonPrev, buttonNext] = document.querySelectorAll(
+  '.slider-container>button'
 );
-const [, img2] = document.images;
-const ul1 = document.querySelector('main>nav>ul');
-const paragraphList1 = document.querySelectorAll('main>article>p');
-const paragraphList2 = document.querySelectorAll('p:not(.text)');
