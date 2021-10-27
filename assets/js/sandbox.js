7b8b21ff83247f542683d83d69e4060a630a8b93
morghull <morghull@gmail.com>
2021-10-27 10:02:51 +0300

    add adder

--- assets/js/sandbox.js
+++ assets/js/sandbox.js
@@ -2,16 +2,31 @@
 
 function createCounter(initial = 0) {
   let i = initial; //closure variable
-  return function counter() {
-    return ++i;
+  return {
+    increment() {
+      return ++i;
+    },
+    decrement() {
+      return --i;
+    },
   };
 }
 
 const counter1 = createCounter(10);
 const counter2 = createCounter();
 
-counter1();
-counter1();
-counter1();
-counter2();
-counter2();
+counter1.increment();
+counter1.increment();
+counter1.decrement();
+counter2.increment();
+counter2.increment();
+
+const createAdder =
+  (initialValue = 0) =>
+  (value) =>
+    (initialValue += value);
+
+const adder1 = createAdder(10);
+console.log(adder1(10));
+console.log(adder1(4));
+console.log(adder1(3));
