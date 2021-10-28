dc17a2a556f6dc148882a2636e437b6e6fa71d2d
morghull <morghull@gmail.com>
2021-10-28 11:53:05 +0300

    add event handlers

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,19 @@
 'use strict';
 
-const imagesDB = [
-  'https://karma-kollection.com/wp-content/uploads/2018/05/magical-banner.jpg',
-  'https://fanaticcook.files.wordpress.com/2021/04/animals.jpg',
-  'https://i.pinimg.com/originals/7e/1c/0b/7e1c0b3223789770299bc3b66b2fc2a0.jpg',
-  'https://thypix.com/wp-content/uploads/2018/05/Sommerlandschaft-Bilder-1.jpg',
-  'https://i.pinimg.com/originals/2c/84/93/2c8493d6251661e82b62fc6ab518e7e3.jpg',
-];
-
-const slider = new Slider(imagesDB);
-
-const image = document.querySelector('.slide>img');
-const [buttonPrev, buttonNext] = document.querySelectorAll(
-  '.slider-container>button'
-);
-
-function updateView() {
-  image.setAttribute('src', slider.currentSlide);
+const btns = document.querySelectorAll('#root>button');
+const root = document.getElementById('root');
+const btnHandler = ({
+  target: {
+    dataset: { color },
+    parentNode: parent,
+  },
+}) => {
+  parent.style.backgroundColor = color;
+};
+const secondClickHandler = (e) => {
+  console.dir(e);
+};
+for (const btn of btns) {
+  btn.addEventListener('click', btnHandler);
+  btn.addEventListener('click', secondClickHandler);
 }
-updateView();
-
-const btnSliderHandler =
-  (direction = 'next') =>
-  () => {
-    slider.currentIndex =
-      slider[direction === 'next' ? 'nextIndex' : 'prevIndex'];
-    updateView();
-  };
-
-buttonNext.addEventListener('click', btnSliderHandler('next'));
-buttonPrev.addEventListener('click', btnSliderHandler('prev'));
-image.addEventListener('wheel', (e) =>
-  btnSliderHandler(e.deltaY > 0 ? 'next' : 'prev')()
-);
-
-//-------------------------------------
-
-// const uniqueImage = document.getElementById('unique');
-
-// const srcAttr = document.createAttribute('src');
-// srcAttr.value = imagesDB[0];
-
-// uniqueImage.setAttributeNode(srcAttr);
