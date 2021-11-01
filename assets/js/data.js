6707a3c05e265566d66fb780f1515b0f32bfe934
morghull <morghull@gmail.com>
2021-11-01 09:22:59 +0200

    add random color and initials functions

--- assets/js/data.js
+++ assets/js/data.js
@@ -11,7 +11,7 @@ const actors = [
     name: 'Robert Downey',
     birthdate: 'April 4, 1965',
     photo:
-      'https://tr.web.img3.acsta.net/c_310_420/pictures/210/017/21001798_20130426172912097.jpgs',
+      'https://tr.web.img3.acsta.net/c_310_420/pictures/210/017/21001798_20130426172912097.jpg',
   },
   {
     id: 3,
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,12 @@
 'use strict';
 
-const cardsContainer = document.getElementById('cardsContainer');
+const cardsContainer = document.getElementById(
+  'cardsContainer'
+);
 
-const HTMLElements = actors.map((actor) => createActorCard(actor));
+const HTMLElements = actors.map((actor) =>
+  createActorCard(actor)
+);
 
 function createActorCard({ photo, name, birthdate }) {
   const cardWrapper = document.createElement('li');
@@ -16,25 +20,36 @@ function createActorCard({ photo, name, birthdate }) {
 
   const cardInitials = document.createElement('div');
   cardInitials.classList.add('cardInitials');
-  cardInitials.append(document.createTextNode(name[0]));
+  cardInitials.append(
+    document.createTextNode(getInitials(name || 'noname'))
+  );
+  cardInitials.style.backgroundColor = stringToColour(name);
 
   const cardImage = document.createElement('img');
   cardImage.setAttribute('src', photo);
-  cardImage.setAttribute('alt', name);
+  cardImage.setAttribute('alt', getInitials(name));
   cardImage.addEventListener('error', handleImageError);
   cardImage.classList.add('cardImage');
 
   cardImageWrapper.append(cardInitials, cardImage);
 
   const cardName = document.createElement('h2');
-  cardName.append(document.createTextNode(name));
+  cardName.append(
+    document.createTextNode(name || 'noname')
+  );
   cardName.classList.add('cardName');
 
   const cardDescription = document.createElement('p');
-  cardDescription.append(document.createTextNode(birthdate));
+  cardDescription.append(
+    document.createTextNode(birthdate || 'unknown')
+  );
   cardDescription.classList.add('cardDescription');
 
-  cardContainer.append(cardImageWrapper, cardName, cardDescription);
+  cardContainer.append(
+    cardImageWrapper,
+    cardName,
+    cardDescription
+  );
   cardWrapper.appendChild(cardContainer);
   return cardWrapper;
 }
@@ -44,3 +59,25 @@ cardsContainer.append(...HTMLElements);
 function handleImageError({ target }) {
   target.remove();
 }
+
+function stringToColour(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let colour = '#';
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xff;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
+}
+
+function getInitials(name) {
+  return name
+    .toUpperCase()
+    .split(' ')
+    .map((item) => item.substr(0, 1))
+    .join('');
+}
+console.log(getInitials('tom soer'));
